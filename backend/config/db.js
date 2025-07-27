5cdb9ce15d8e1982b9da9e7568d7bc272697498e
SaranyaKannan28 <saranyakannan1111@gmail.com>
2025-07-27 11:25:48 +0530

    Setup and Run Database Migration

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'your_db',
+    database: process.env.DB_NAME || 'salary_management',
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',  // 👈 this must be a string, NOT an object
+    dialect: 'mysql'
   }
 };
